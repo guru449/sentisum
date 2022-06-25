8924c17c67370773e325b3cc4ea9332a1c818cd7
guru449 <44187639+guru449@users.noreply.github.com>
2022-06-25 11:52:46 +0530

    Initial commit

---
+++ src/main/java/com/example/sentisum/SentisumApplication.java
@@ -0,0 +1,13 @@
+package com.example.sentisum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentisumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentisumApplication.class, args);
+	}
+
+}
